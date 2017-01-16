62e55ebc046dc8f3d9b4794fe89bd70509d66efb
Mert Tuna Ozerdem <tuna.ozerdem@gmail.com>
2017-01-15 19:02:34 -0500

    Added files for the second exercise

---
+++ Logistic Regression/plotData.m
@@ -0,0 +1,35 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+%  Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+admitted_index = find(y==1)
+declined_index = find(y==0)
+
+    
+
+
+
+plot(X(admitted_index, 1), X(admitted_index, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+     
+plot(X(declined_index, 1), X(declined_index, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
+     
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
